d25d6a0bccb4ec63d7a033a277fb3cf0c1ec1f3d
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-22 16:19:57 +0200

    prima parte completata

--- js/main.js
+++ js/main.js
@@ -1,43 +1,7 @@
-const images = [
-    {
-        src: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        src: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum',
-    },
-    {
-        src: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        src: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-    {
-        src: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-];
-Descrizione:
-// Partendo dal markup HTML allegato creare uno slider di immagini in VueJS.
-// Bonus:
-// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
-// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
-// Consigli del giorno:
-// - regola d'oro: riciclare ovunque possibile!
-// - il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe ;)
-
-let app = new Vuel ({
+const app = new Vue({
     el: '#root',
-    data:{
+    data: {
+        currentIndex: 0,
         images: [
             {
                 src: 'img/01.jpg',
@@ -67,6 +31,28 @@ let app = new Vuel ({
         ],
     },
     methods: {
-        
-    }
+        nextImage(){
+            if(this.currentIndex === this.images.length - 1){
+                this.currentIndex = 0;
+            } else {
+            this.currentIndex++;
+            }
+        },
+        prevImage(){
+            if(this.currentIndex=== 0){
+                this.currentIndex = this.images.length -1;
+            }else{
+                this.currentIndex--;
+            }
+        },
+    },    
 });
+// Descrizione:
+// Partendo dal markup HTML allegato creare uno slider di immagini in VueJS.
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+// Consigli del giorno:
+// - regola d'oro: riciclare ovunque possibile!
+// - il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe ;)
